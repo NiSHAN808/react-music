dc12673797d40a70f61de01e0a1480e32ea71757
NiSHAN808 <nishan4b@gmail.com>
2025-04-14 17:54:39 +0545

    intitial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>  <h1 className="text-purple-500 bg-blue-800">sham </h1>
+ hellow
+    </>
+  )
+}
+
+export default App
