acf277fbcddc9fcc3f942e32a2a279c53e337a93
NiSHAN808 <nishan4b@gmail.com>
2025-04-19 14:47:10 +0545

    we can play a music

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
-import { useState } from 'react'
+
 
 
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>  
-    <div className='inline-block h-[10rem] w-[10rem] bg-pink-300'>
-
+    <div className='inline-flex  h-[10rem] w-[10rem] bg-pink-300 '>
+<img src="./music/p.png" height="10px" width="10px"/>
     </div>
- 
+    <audio controls >
+  <source src="./music/ikykissmeXlady.mp3" type="audio/mp3"/>
+
+Your browser does not support the audio element.
+</audio>
     </>
   )
 }
