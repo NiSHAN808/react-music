ff759377d7a02a588d0a59709f2e7b4556bbd121
NiSHAN808 <nishan4b@gmail.com>
2025-04-19 11:26:40 +0545

    setup

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,7 @@ function App() {
   return (
     <>  
     <div className='inline-block h-[10rem] w-[10rem] bg-pink-300'>
-     <div className='inline-block bg-blue-300 h-[2rem] w-full'>
-      <h3>༺</h3>
-       <div className='inline-block bg-yellow-300 h-[1.8rem] w-[1.8rem]'>
 
-       </div>
-
-     </div>
     </div>
  
     </>
