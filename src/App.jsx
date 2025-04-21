eb930af81eab056350a187b09119b3929143e69e
NiSHAN808 <nishan4b@gmail.com>
2025-04-21 23:53:06 +0545

    basic temp

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-
+let data=[{music:"./music/ikykissmeXlady.mp3",name:"i know you wana kiss me X lady killer"}]
 
 
 function App() {
@@ -9,9 +9,11 @@ function App() {
     <>  
     <div className='inline-flex  h-[10rem] w-[10rem] bg-pink-300 '>
 <img src="./music/p.png" height="10px" width="10px"/>
+
     </div>
+    <h1>{data[0].name}</h1>
     <audio controls >
-  <source src="./music/ikykissmeXlady.mp3" type="audio/mp3"/>
+  <source src={data[0].music} type="audio/mp3"/>
 
 Your browser does not support the audio element.
 </audio>
